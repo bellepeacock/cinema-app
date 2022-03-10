4060f1867184e15a38d9f151c4facd0367eae5c3
ricomasgu <ricard.comas.gomez@gmail.com>
2022-03-10 11:42:28 +0100

    Improved liked functionality

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,11 @@
+function changeHeartIcon(iconClasses){
+    iconClasses.value === "fas fa-heart liked" ? iconClasses.value = "far fa-heart" : iconClasses.value = "fas fa-heart liked";
+}
+
+[...document.getElementsByTagName("h4")].forEach( h4 => h4.addEventListener( "click", event => {
+    changeHeartIcon(event.target.childNodes[1].classList);
+}));
+
 [...document.getElementsByClassName("fa-heart")].forEach( heart => heart.addEventListener( "click", event => {
-    const iconClasses = event.target.classList;
-    iconClasses.value === "fas fa-heart liked" ? iconClasses.value = "far fa-heart" : iconClasses.value = "fas fa-heart liked"
+    changeHeartIcon(event.target.classList);
 }));
