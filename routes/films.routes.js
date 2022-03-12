6363ee76ebe56cd5313b2cdc0ab4a42d987fcd93
Annabelle Peacock <aspeacock92@gmail.com>
2022-03-12 18:06:12 +0100

    fixing main

--- routes/films.routes.js
+++ routes/films.routes.js
@@ -5,52 +5,6 @@ const Film = require('../models/Film.model');
 
 const apiIS = new ApiIS();
 
-<<<<<<< HEAD
-// router.get("/films/:id", async (req, res, next) => {
-//     const filmId = req.params.id;
-//     try {
-//         const resFromApi = await apiIS.getFilm(filmId);
-//         const film = resFromApi.data.movie;
-//         const cast = [...film.cast].slice(0,10);
-//         film.cast = cast;
-//         res.render("film-views/film-details", { film } );
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
-
-// module.exports = router;
-// films/:id/details
-
-
-const Film = require('../models/Film.model');
-const Comment = require('../models/Comment.model');
-
-
-router.get("/films/:id", (req, res) => {
-    const { id } = req.params;
-    console.log(id)
-    Film.findById(id)
-        .then(film => {
-            console.log(film)
-            res.render('film-views/film-details', { film })
-        }).catch(e => console.error(e))
-})
-
-router.get('/:id', (req, res, next) => {
-    const { id } = req.params;
-
-    Film.findById(id)
-        .populate('comments')
-        .then((filmById) => {
-            Comment.find()
-            .then((comment) => {res.render('/films/:id', {filmById, film})
-            });
-        }).catch(error => console.error(error))
-});
-
-
-=======
 // films/:id/details
 router.get("/films/:id", async (req, res, next) => {
     const filmId = req.params.id;
@@ -66,8 +20,12 @@ router.get("/films/:id", async (req, res, next) => {
 
         //Obtaining the info of the comments
         Film.findOne({ id: filmId })
-            .populate('comments')
-            .populate('username')
+            .populate({
+                path : 'comments',
+                populate : {
+                  path : 'username'
+                }
+              })
             .then((filmFromDbWithComments) => {
                 res.render("film-views/film-details", { filmFromAPI , filmFromDbWithComments } );
             })
@@ -78,7 +36,6 @@ router.get("/films/:id", async (req, res, next) => {
     }
 });
 
->>>>>>> c2a8efc (We modified the atributes of the models, merge the content of the films.routes)
 //check case for keys
 // router.post('/films/:id', (req, res, next) => {
 //     const { username, content } = req.body;
