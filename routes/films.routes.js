a2f16a10688f093fe68f74818479c37437c8146d
Annabelle Peacock <aspeacock92@gmail.com>
2022-03-12 14:58:50 +0100

    waiting to fix routes

--- routes/films.routes.js
+++ routes/films.routes.js
@@ -3,23 +3,22 @@ const ApiIS = require("../services/is-api.service");
 
 const apiIS = new ApiIS();
 
-router.get("/films/:id", async (req, res, next) => {
-    const filmId = req.params.id;
-    try {
-        const resFromApi = await apiIS.getFilm(filmId);
-        const film = resFromApi.data.movie;
-        const cast = [...film.cast].slice(0,10);
-        film.cast = cast;
-        res.render("film-views/film-details", { film } );
-    } catch (error) {
-        console.log(error);
-    }
-});
+// router.get("/films/:id", async (req, res, next) => {
+//     const filmId = req.params.id;
+//     try {
+//         const resFromApi = await apiIS.getFilm(filmId);
+//         const film = resFromApi.data.movie;
+//         const cast = [...film.cast].slice(0,10);
+//         film.cast = cast;
+//         res.render("film-views/film-details", { film } );
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
 // films/:id/details
 
-const router = require("express").Router();
 
 const Film = require('../models/Film.model');
 const Comment = require('../models/Comment.model');
